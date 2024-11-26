af6dec6f7ad772478eae669d8a18fdfa0e480bcb
Supun Prasad <134516059+Supun009@users.noreply.github.com>
2024-11-26 22:38:45 +0530

    front end

--- frontend/todo/src/pages/auth_pages/Loginpage.jsx
+++ frontend/todo/src/pages/auth_pages/Loginpage.jsx
@@ -1,4 +1,52 @@
+import InputFeild from "../../components/authcomponents/InputFeild";
+import AuthButton from "../../components/authcomponents/AuthButton";
+import { useState, useRef } from 'react';
+import validaotor from 'validator';
+import DOMPurify from 'dompurify';
+
+
 export default function LoginPage() {
+
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const [error, setError] = useState({
+        username: "",
+        password: "",
+    });
+
+
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+
+        const username = usernameRef.current.value.replace(/\s/g, "");;
+        const password = passwordRef.current.value.replace(/\s/g, "");;
+
+        let errors = {}
+
+        if (!username || username.length < 5) {
+            errors.username = "Please enter a valid username";
+        }
+
+        if (!password || password.length < 6)  {
+            errors.password = "Please enter a stronge passwordPassword should be at least 6 characters long";
+        }
+
+        if (Object.keys(errors).length > 0) {
+            setError(errors)
+            return;
+        }
+
+        const sanitizedUsername = DOMPurify.sanitize(username); 
+        const sanitizedPassword = DOMPurify.sanitize(password);
+
+        const data = { username: sanitizedUsername, password: sanitizedPassword };
+        console.log('Sanitized Data:', data);
+
+    }
+    
+
+
     return (
         <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
             <header className="mb-8">
@@ -8,35 +56,24 @@ export default function LoginPage() {
             </header>
             
             <section className="w-full max-w-md px-4">
-                <form className="flex flex-col items-center ">
+                <form onSubmit={handleSubmit} className="flex flex-col items-center ">
                     
-                    <div className="w-full mb-4">
-                        <label htmlFor="username" className="block mb-2 text-gray-700">Username</label>
-                        <input 
-                            type="text" 
-                            id="username" 
-                           
-                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
-                    <div className="w-full mb-4">
-                        <label htmlFor="password" className="block mb-2 text-gray-700">Password</label>
-                        <input 
-                            type="password" 
-                            id="password" 
-                            
-                            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                        />
-                    </div>
+                    <InputFeild name = "username" type = "text" htmlFor ="username" id="username" label="Username" refer={usernameRef}/>
+                    {/* Display username error message */}
+                    {error.username && (
+                            <p className="text-red-500 text-sm mt-1">
+                                {error.username}
+                            </p>
+                        )}
 
-                    <div className="w-full mb-4">
-                        <button
-                            type="submit"
-                            className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
-                        >
-                            Sign Up
-                        </button>
-                    </div>
+                    <InputFeild name = "password" type = "password" htmlFor ="password" id="password" label="Password" refer={passwordRef}/>
+                    {error.password && (
+                            <p className="text-red-500 text-sm mt-1">
+                                {error.password}
+                            </p>
+                        )}
+                        
+                    <AuthButton buttonName = "Login"/>
 
                     <div className="text-center">
                         <p className="text-gray-600">
@@ -65,4 +102,5 @@ export default function LoginPage() {
             </section>
         </div>
     )
-}
+    }
+
